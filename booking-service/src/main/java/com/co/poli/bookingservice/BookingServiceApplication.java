89b7c4b37d7cd9d258a32f4e5c6822561a163b40
Aldair-Meza <amezasilgado@gmail.com>
2022-07-26 23:31:18 -0500

    se suben avances del proyecto

---
+++ booking-service/src/main/java/com/co/poli/bookingservice/BookingServiceApplication.java
@@ -0,0 +1,9 @@
+package com.co.poli.bookingservice;
+
+import io.micronaut.runtime.Micronaut;
+
+public class BookingServiceApplication {
+    public static void main(String[] args) {
+        Micronaut.run(BookingServiceApplication.class, args);
+    }
+}
